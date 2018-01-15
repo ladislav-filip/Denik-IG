3cba5a02fe2c35b61d90276c1946cc49308313a3
LF <ladislav.filip@fullsys.cz>
2018-01-15 17:15:12 +0100

    Presenter pro evidenci

---
+++ app/PrivateModule/presenters/EvidencePresenter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ladis
+ * Date: 1/15/2018
+ * Time: 17:10
+ */
+
+namespace App\PrivateModule\Presenters;
+
+
+class EvidencePresenter extends BasePresenter
+{
+
+}
